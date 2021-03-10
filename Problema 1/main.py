0b6fd931850ad49ff429d6f088808deec79bbbc9
Iago <iagomachadocs@gmail.com>
2021-03-10 17:11:26 -0300

    Remove unsued functions

--- Problema 1/main.py
+++ Problema 1/main.py
@@ -2,45 +2,6 @@ from os import listdir, path, mkdir, remove
 import re
 from lexical_analyser import LexicalAnalyser
 
-
-def removeLineComment(line):
-    match = re.search(r'\/\/.*', line)
-    if(match):
-        return line[:match.start()]
-    else:
-        return line
-
-def removeMultiLineComments(lines):
-    for i in range(len(lines)):
-        match = re.search(r'\/\*.*\*\/', lines[i])
-        if(match): # Comentário de bloco que inicia e termina na mesma linha
-            lines[i] = lines[i][:match.start()] + lines[i][match.end():]
-        else:
-            match = re.search(r'\/\*.*', lines[i])
-            if(match): # Comentário de bloco que inicia em uma linha e termina em outra
-                comment = lines[i][match.start():]
-                lines[i] = lines[i][:match.start()]
-                j = i+1
-                commentClosed = False
-                while j < len(lines) and not commentClosed:
-                    match = re.search(r'.*\*\/', lines[j])
-                    if(match):
-                        comment = comment+'\t'+lines[j][:match.end()]
-                        lines[j] = lines[j][match.end():]
-                        commentClosed = True
-                    else:
-                        comment = comment+'\t'+lines[j]
-                        lines[j] = ''
-                        j += 1
-                if(not commentClosed):
-                    errors.append({"line": str(i+1), "type": "CoMF", "lexeme": comment})
-                i=j
-    return lines
-
-def writeErrors(output):
-    for error in errors:
-        output.write(error["line"]+' '+error["type"]+' '+error["lexeme"])
-
 # Filtra arquivos no diretorio de entrada
 files = []
 p = re.compile(r'entrada\d+.txt', re.IGNORECASE)
